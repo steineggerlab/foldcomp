79ef402818c51a6dafe49ef6dba570b556607e06
Hyunbin Kim <khb7840@gmail.com>
2023-03-03 18:37:57 +0900

    [FIXING] Trying to solve python build error

--- src/database_reader.h
+++ src/database_reader.h
@@ -4,8 +4,7 @@
  * Author: Milot Mirdita (milot@mirdita.de)
  */
 
-#ifndef DATABASE_READER_H
-#define DATABASE_READER_H
+#pragma once
 #include <cstdint>
 
 static const int DB_READER_USE_DATA   = 1u << 0;
@@ -22,5 +21,3 @@ int64_t reader_get_length(void *reader, int64_t id);
 int64_t reader_get_offset(void *reader, int64_t id);
 int64_t reader_get_size(void *r);
 uint32_t reader_lookup_entry(void* r, const char* name);
-
-#endif
