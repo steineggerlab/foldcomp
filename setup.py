c0a51b267699fea26c4eed31db5b82cab6bbc76c
Hyunbin Kim <khb7840@gmail.com>
2023-12-29 13:56:43 +0900

    Update version number in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from skbuild import setup
 
 setup(
     name="foldcomp",
-    version="0.0.5",
+    version="0.0.6",
     description="Foldcomp compresses protein structures with torsion angles effectively. It compresses the backbone atoms to 8 bytes and the side chain to additionally 4-5 byes per residue, an averaged-sized protein of 350 residues requires ~4.2kb. Foldcomp is a C++ library with Python bindings.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
